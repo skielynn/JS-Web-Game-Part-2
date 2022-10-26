3f3731e54fb39073096478b6f14278f3ed6c4820
Skie <skielynn.1105945@gmail.com>
2022-10-26 14:24:04 -0700

    changes

--- index.js
+++ index.js
@@ -7,13 +7,18 @@ function newImage(url, left, bottom){
     document.body.append(object)
     return object
 }
-
+let inventory = document.createElement('div')
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
+    item.addEventListener("click", function() {
+        item.remove()
+        let inventoryItem = document.createElement("img")
+        inventoryItem.src = url
+        inventory.append(inventoryItem)
+    })
 }
 
 function newInventory(){
-    let inventory = document.createElement('div')
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
     inventory.style.left = '0px'
